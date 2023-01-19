980b4119a692e0e5b4d284f5526955e67c1143ab
Дмитрий Ширяев <dshiryaev@srt-llc.ru>
2023-01-19 12:54:39 +0600

    2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('facebook');
 const btn1 = document.getElementById('telegram');
+const btn2 = document.getElementById('vk');
+const btn3 = document.getElementById('od');
 
 const handler = () => {
     window.open(
@@ -16,5 +18,23 @@ const handler1 = () => {
     )
 }
 
+const handler2 = () => {
+    window.open(
+        'https://vk.com/share.php?url=' + location.href,
+        'share',
+        'left=-50%,top=-50%,width=600,height=400',
+    )
+}
+
+const handler3 = () => {
+    window.open(
+        'https://connect.ok.ru/offer?url=' + location.href,
+        'share',
+        'left=-50%,top=-50%,width=600,height=400',
+    )
+}
+
 btn.addEventListener('click', handler)
 btn1.addEventListener('click', handler1)
+btn2.addEventListener('click', handler2)
+btn3.addEventListener('click', handler3)
